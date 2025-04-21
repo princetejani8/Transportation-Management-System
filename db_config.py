be544acc814b4614fc695f7c88a31ac63b34cf38
Prince <167392451+princetejani8@users.noreply.github.com>
2025-04-21 11:00:32 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Prince@123",
-        database="tms"
+        password="Your_Password",
+        database="database"
     )
